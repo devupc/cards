417f35a80dcb2194e25971dab97f608172afdb6a
herlad matias olivares zarsoza <heraldmatias.oz@gmail.com>
2013-09-22 20:54:38 -0500

    home

---
+++ src/Upc/Cards/Bundle/CardsBundle/Controller/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Upc\Cards\Bundle\CardsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('CardsBundle:Home:index.html.twig', array('name' => '$name'));
+    }
+}
